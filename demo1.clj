8995e50e09693228547588b96845a4fd330a17bd
awb99 <10854682+awb99@users.noreply.github.com>
2020-05-20 19:23:38 -0500

    Update demo1.clj

--- demo1.clj
+++ demo1.clj
@@ -1,9 +1,19 @@
-{:state 0
-  :html 
+(ns demo1
+  (:require 
+    [shiny]))
+
+
+
+(shiny/start! 
+  {:state 0
+   :html 
      [:div "Clicked " 
        [:button {:on-click ?incr} ?state] 
        " times"] 
    :fns {:incr (fn [_ s] (inc s))}}
+  {:port 9000})
+
+
 
 
 
