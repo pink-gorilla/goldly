faa402e8aa3d944b3f6a5e36189a8614fdee864f
awb99 <andreas.wolfgang.bauer@gmail.com>
2020-05-21 00:02:41 -0500

    package.json

--- src/shiny/web.cljs
+++ src/shiny/web.cljs
@@ -1,23 +1,69 @@
 (ns shiny.web
+  (:require-macros [secretary.core :refer [defroute]])
+  (:import
+   [goog History]
+    ;; [goog.history Html5History]
+   )
   (:require
    [cljs.pprint]
    [reagent.core :as r]
    [reagent.dom]
+   [taoensso.timbre :refer-macros (info debug)]
+   [secretary.core :as secretary]
+   [goog.events :as events]
+   [goog.history.EventType :as EventType]
+
    [pinkgorilla.ui.pinkie :refer [tag-inject renderer-list]]
    ; add dependencies of this project to bundle
    [pinkgorilla.ui.default-renderer]
    [shiny.core]))
 
+(defn hook-browser-navigation!
+  []
+  (doto (History.)
+    (events/listen
+     EventType/NAVIGATE
+     (fn [event]
+       (secretary/dispatch! (.-token event))))
+    (.setEnabled true)))
 
-(defn routes []
-  {:status "running system stats"
-   :systems "id or route-name parameter displays system"})
+(defonce history (hook-browser-navigation!))
+
+(defn   nav!
+  "navigates the browser to the url. 
+   Triggers secretary route events"
+  [url]
+  (.setToken history url))
+
+
+(defonce current (reagent.core/atom {:page :info
+                                     :system nil}))
+
+(defn app-routes
+  [& [{:keys [hook-navigation]
+       :or   {hook-navigation false}}]]
+  (info "Hook navigation" hook-navigation)
+  (secretary/set-config! :prefix "#")
+  (defroute "/info" []
+    (swap! current assoc :page :info))
+  (defroute "/system" [id]
+    (swap! current assoc :page :system
+           :system id)))
+
+(defn infos []
+  [:h1 "infos"])
+
+(defn system [id]
+  [:h1 "system: " id])
 
 
 (defn app []
-  [:h1 "shiny"]
-  )
-
+  (fn []
+    (let [current @current
+          id (:system current)]
+      (case (:type current)
+        :info [infos]
+        :system [system id]))))
 
 (defn stop []
   (js/console.log "Stopping..."))
@@ -25,6 +71,8 @@
 (defn start []
   (js/console.log "Starting...")
   ;(js/console.log (print-registered-tags))
+  (app-routes)
+
   (reagent.dom/render (tag-inject app)
                       (.getElementById js/document "app")))
 
