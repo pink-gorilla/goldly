6c41c5da4a7ada71a4b628d9dafa7c54fe429b36
awb99 <andreas.wolfgang.bauer@gmail.com>
2020-05-21 19:15:06 -0500

    cljfmt

--- src/shiny/demo1.clj
+++ src/shiny/demo1.clj
@@ -1,18 +1,17 @@
 (ns shiny.demo1
-  (:require 
-    [shiny.core :as shiny]
-    [shiny.web :as shinyweb])
+  (:require
+   [shiny.core :as shiny]
+   [shiny.web :as shinyweb])
   (:gen-class))
 
 
-(def s (shiny/system 
-  {:state 0
-          :html  [:div "Clicked " 
-                   [:button {:on-click ?incr} ?state] 
-                      " times"] 
-          :fns {:incr (fn [_ s] (inc s))}}
-   {:fns {:incr10  (fn [_ s] (+ s 10)) }}
-  ))
+(def s (shiny/system
+        {:state 0
+         :html  [:div "Clicked "
+                 [:button {:on-click ?incr} ?state]
+                 " times"]
+         :fns {:incr (fn [_ s] (inc s))}}
+        {:fns {:incr10  (fn [_ s] (+ s 10))}}))
 
 (defn -main []
   (shinyweb/server-start! {:port 8000})
