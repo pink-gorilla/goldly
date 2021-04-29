7c37057c4e3ebbc003fd0ec61755b2253aeb7c0a
CI/CD <ci@pinkgorilla.org>
2021-04-29 03:05:59 +0000

    Release 0.2.19

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.pinkgorilla/goldly "0.2.19-SNAPSHOT"
+(defproject org.pinkgorilla/goldly "0.2.19"
   :description "reactive html ui with clj-cljs interop"
   :license {:name "MIT"}
   :deploy-repositories [["releases" {:url "https://clojars.org/repo"
