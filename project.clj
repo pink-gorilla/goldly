7d6db299a446ce6a2e350ffc687d639dce87061e
CI/CD <ci@pinkgorilla.org>
2021-05-04 20:28:04 +0000

    Begin 0.2.26-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.pinkgorilla/goldly "0.2.25"
+(defproject org.pinkgorilla/goldly "0.2.26-SNAPSHOT"
   :description "reactive html ui with clj-cljs interop"
   :license {:name "MIT"}
   :deploy-repositories [["releases" {:url "https://clojars.org/repo"
