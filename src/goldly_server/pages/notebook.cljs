a0eae30b5f969249e44e92211af5fd4d93182984
awb99 <andreas.wolfgang.bauer@gmail.com>
2021-05-26 21:32:01 -0500

    webly dep bump

--- src/goldly_server/pages/notebook.cljs
+++ src/goldly_server/pages/notebook.cljs
@@ -2,21 +2,20 @@
   (:require
    [re-frame.core :as rf]
    [webly.web.handler :refer [reagent-page]]
-
    [picasso.default-config] ; side-effects
    [picasso.data.document :as data]
-   [picasso.kernel.view.picasso]
-   [picasso.kernel.view.default-painter] ; side-effects
-   [picasso.document.transactor] ;side-effects
-   [picasso.document.position] ;side-effects
+   ;[picasso.kernel.view.picasso]
+   ;[picasso.kernel.view.default-painter] ; side-effects
+   ;[picasso.document.transactor] ;side-effects
+   ;[picasso.document.position] ;side-effects
    ; ui
-   [ui.markdown.goldly.core] ;side-effects
-   [ui.markdown.viewer]
-   [ui.code.goldly.core] ;side-effects
-   [ui.notebook.core :refer [notebook-view]]))
+   ;[ui.markdown.goldly.core] ;side-effects
+   ;[ui.markdown.viewer]
+   ;[ui.code.goldly.core] ;side-effects
+   [ui.notebook.core :refer [notebook-view]]
+   [ui.notebook.menu]))
 
-(rf/dispatch [:doc/add data/document])
-(rf/dispatch [:doc/doc-active (:id data/document)])
+(rf/dispatch [:doc/load data/document])
 
 (def opts
   {; if a layout option is passed this will override the settings in localstorage
@@ -24,4 +23,6 @@
    :view-only true})
 
 (defmethod reagent-page :notebook/current [{:keys [route-params query-params handler] :as route}]
-  [notebook-view opts])
+  [:div
+   [ui.notebook.menu/menu]
+   [notebook-view opts]])
