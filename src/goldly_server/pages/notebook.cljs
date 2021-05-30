183f3ec0a369fc9eb6b405ff79fe3b9bbae62186
awb99 <andreas.wolfgang.bauer@gmail.com>
2021-05-30 16:03:51 -0500

    dep bump picasso

--- src/goldly_server/pages/notebook.cljs
+++ src/goldly_server/pages/notebook.cljs
@@ -3,19 +3,11 @@
    [re-frame.core :as rf]
    [webly.web.handler :refer [reagent-page]]
    [picasso.default-config] ; side-effects
-   [picasso.data.document :as data]
-   ;[picasso.kernel.view.picasso]
-   ;[picasso.kernel.view.default-painter] ; side-effects
-   ;[picasso.document.transactor] ;side-effects
-   ;[picasso.document.position] ;side-effects
-   ; ui
-   ;[ui.markdown.goldly.core] ;side-effects
-   ;[ui.markdown.viewer]
-   ;[ui.code.goldly.core] ;side-effects
+   [picasso.data.notebook :as data]
    [ui.notebook.core :refer [notebook-view]]
    [ui.notebook.menu]))
 
-(rf/dispatch [:doc/load data/document])
+(rf/dispatch [:doc/load data/notebook])
 
 (def opts
   {; if a layout option is passed this will override the settings in localstorage
