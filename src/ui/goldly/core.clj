7a1a6211a6a56739a140ebd0b9d5f7de19139623
awb99 <andreas.wolfgang.bauer@gmail.com>
2021-06-20 22:33:11 -0500

    removed site

--- src/ui/goldly/core.clj
+++ src/ui/goldly/core.clj
@@ -2,13 +2,13 @@
   (:require
    [taoensso.timbre :as timbre :refer [info warn error]]
    [goldly.sci.bindings :refer [add-cljs-namespace add-cljs-bindings add-cljs-ns-bindings generate-bindings]]
-   [pinkgorilla.repl.goldly.core] ; side-effects
-   [pinkgorilla.repl.goldly.snippets] ; side-effects
+   ;[pinkgorilla.repl.goldly.core] ; side-effects
+   ;[pinkgorilla.repl.goldly.snippets] ; side-effects
    ))
 
 ; cljs ui
-(add-cljs-namespace [ui.site.goldly.core]) ; web-site templates
-(add-cljs-namespace [ui.code.goldly.core]) ; codemirror
+;(add-cljs-namespace [ui.site.goldly.core]) ; web-site templates
+;(add-cljs-namespace [ui.code.goldly.core]) ; codemirror
 
 (add-cljs-namespace [clojure.walk])
 (add-cljs-ns-bindings
