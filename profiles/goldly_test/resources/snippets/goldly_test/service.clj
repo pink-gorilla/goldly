302e012c76f914a6a9878419ae71060cdf6789df
awb99 <andreas.wolfgang.bauer@gmail.com>
2021-06-29 13:20:33 -0500

    fixes #3

--- profiles/goldly_test/resources/snippets/goldly_test/service.clj
+++ profiles/goldly_test/resources/snippets/goldly_test/service.clj
@@ -9,17 +9,26 @@
 (defn fun-quote []
   "The early fish catches the worm.")
 
-(s/add {:demo/add fun-add})
-(s/add {:demo/quote fun-quote})
+(defn fun-ex []
+  (throw (Exception. "something bad happened")))
+
+(s/add {:demo/add fun-add
+        :demo/quote fun-quote
+        :demo/ex fun-ex
+        })
 
 (system-start!
  (goldly/system
   {:id :service-test
    :hidden true
-   :state {:result "no result."}
+   :state {:first true}
    :html  [:div
-           (run-a state [:result] :demo/add 2 7)
-           (run-a state [:saying] :demo/quote)
+           (when (:first @state)
+             (swap! state assoc :first false)
+             (run-a state [:add-result] :demo/add 2 7)
+             (run-a state [:saying] :demo/quote)
+             (run-a state [:ex-result] :demo/ex)
+             nil)
            [:p "this tests if clj services are working"]
            [:p.bg-blue-300.mg-3 "state: " (pr-str @state)]]
    :fns {}
--- src/goldly/service/core.cljs
+++ src/goldly/service/core.cljs
@@ -1,14 +1,11 @@
 (ns goldly.service.core
   (:require
-   [re-frame.core :as rf]
-   [taoensso.timbre :refer-macros [trace debug debugf info infof warnf errorf]]
+   [taoensso.timbre :refer-macros [trace debug debugf info infof warnf error errorf]]
    [webly.ws.core :refer [send!]]
+   [webly.user.notifications.core :as n]
    [goldly.service.result :refer [update-atom-where]]))
 
 (defn print-result [[event-type data]]
-  (warnf "service result rcvd: type: %s data: %s" event-type data))
-
-(defn update-atom-result [a path [event-type data]]
   (warnf "service result rcvd: type: %s data: %s" event-type data))
 
 (defn run [{:keys [fun args timeout cb]
@@ -20,10 +17,14 @@
     (send! [:goldly/service p-clean] cb timeout)
     nil))
 
+(defn process-error [data]
+  (error "error in clj-service: " data)
+  (n/add-notification :danger (pr-str data)))
+
 (defn run-a [a path fun & args]
-  (let [on-result (fn [[event-type data]]
+  (let [on-result (fn [[_ data]] ;  _ = event-type
                     (let [{:keys [result error]} data]
                       (if error
-                        (error "error in clj-service: " data)
+                        (process-error data)
                         (update-atom-where a path result))))]
     (run {:fun fun :args args :cb on-result})))
