3fa308b0b47cd353b1fe79254912aa4761506d0f
awb99 <andreas.wolfgang.bauer@gmail.com>
2021-11-11 18:29:55 +0100

    no-page link to devtools

--- src/goldly/page/no_page.cljs
+++ src/goldly/page/no_page.cljs
@@ -1,11 +1,15 @@
 (ns goldly.page.no-page
   (:require
-   [webly.web.handler :refer [reagent-page]]))
+   [webly.web.handler :refer [reagent-page]]
+   [re-frame.core :as rf]))
 
 (defn no-page []
   [:div.m-10.p-10.bg-blue-300.border.border-round.border-red-600
-   [:p.text-2xl.text-red-800.mb-10 "Here should be your app!"]
-   [:p "Your config needs to include [:goldly :routes] {:app {\"\"} :api {} }"]])
+   [:p.text-2xl.text-red-800.mb-10 "Here could be your app!"]
+   [:p "Your config needs to include [:goldly :routes] {:app {\"/\" :your-page } :api {} }"]
+   [:p.m-2.border.bg-blue-500.w-32
+    [:a {:on-click #(rf/dispatch [:bidi/goto :devtools])}
+     "Goto Devtools"]]])
 
 (defmethod reagent-page :goldly/no-page [{:keys [route-params query-params handler] :as route}]
   [no-page])
