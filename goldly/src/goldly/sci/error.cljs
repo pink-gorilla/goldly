88f6afe936b9139b5f55601b269c61f240f3b4a7
awb99 <andreas.wolfgang.bauer@gmail.com>
2022-05-18 19:28:24 +0200

    cljfmt fix

--- goldly/src/goldly/sci/error.cljs
+++ goldly/src/goldly/sci/error.cljs
@@ -10,20 +10,18 @@
 ;                   :phase "analysis"}
 ;         :err "Could not resolve symbol: bongotrott"}}
 
-
 (defn sci-error [error]
   (let [{:keys [err root-ex]} error
         {:keys [type line column file phase]} root-ex]
     [:div.inline-block
-       [:p.text-red-500.text-bold err]
-       (when root-ex 
-         [:p "phase: " phase " type: " type])
-       (when root-ex 
-         [:p "file: " file "line: " line " column: " column])
-    ]))
+     [:p.text-red-500.text-bold err]
+     (when root-ex
+       [:p "phase: " phase " type: " type])
+     (when root-ex
+       [:p "file: " file "line: " line " column: " column])]))
 
 (defn error-view [filename error]
-  [:div.inline-block 
+  [:div.inline-block
    [:p "sci cljs compile error in file: " filename]
    [sci-error error]])
 
