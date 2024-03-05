668ea0622e9a2e56d39f7a2073884cea1146be3a
awb99 <awb99@gmail.com>
2024-03-04 20:41:57 -0500

    ci updated

--- goldly-docs/build.clj
+++ goldly-docs/build.clj
@@ -4,33 +4,58 @@
    [clojure.java.io :as io]
    [clojure.string :as str]
    [clojure.tools.build.api :as b]
-   [org.corfield.build :as bb] ; https://github.com/seancorfield/build-clj
    [deps-deploy.deps-deploy :as dd]
    [modular.date :refer [now-str]]))
 
 (def lib 'org.pinkgorilla/goldly-docs)
 (def version (format "0.4.%s" (b/git-count-revs nil)))
+(def class-dir "target/classes")
+(def basis (b/create-basis {:project "deps.edn"}))
+(def jar-file (format "target/%s-%s.jar" (name lib) version))
 
-(defn jar "build the JAR" [opts]
-  (println "Building the JAR")
-  (spit (doto (fs/file "resources/META-INF/pink-gorilla/goldly-docs/meta.edn")
+
+(def pom-template
+  [[:licenses
+    [:license
+     [:name "Eclipse Public License"]
+     [:url "https://www.eclipse.org/legal/epl-v10.html"]]]
+   [:developers
+    [:developer
+     [:name "pink-gorilla"]]]
+   [:scm
+    [:url "https://github.com/pink-gorilla/goldly"]
+    [:connection "scm:git:git://github.com/pink-gorilla/goldly.git"]
+    [:developerConnection "scm:git:ssh://git@github.com/pink-gorilla/goldly.git"]]])
+
+(defn- spit-version []
+  (spit (doto (fs/file "target/classes/META-INF/pink-gorilla/goldly-docs/meta.edn")
           (-> fs/parent fs/create-dirs)) {:module-name "goldly-docs"
                                           :version version
-                                          :generated (now-str)})
-  (-> opts
-      (assoc :lib lib
-             :version version
-             :transitive true)
-      ;(bb/run-tests)
-      ;(bb/clean)
-      (bb/jar)))
+                                          :generated (now-str)
+                                          }))
 
-(defn deploy "Deploy the JAR to Clojars." [opts]
-  (println "Deploying to Clojars.")
-  (-> opts
-      (assoc :lib lib
-             :version version)
-      (bb/deploy)))
 
+(def opts {:class-dir class-dir
+           :lib lib
+           :version version
+           :basis basis
+           :pom-data pom-template
+           :src-dirs ["src"]})
+
+(defn jar [_]
+  (b/write-pom opts)
+  (b/copy-dir {:src-dirs ["src" "resources"]
+               :target-dir class-dir})
+  (spit-version)
+  (b/jar {:class-dir class-dir
+          :jar-file jar-file}))
+
+(defn deploy "Deploy the JAR to Clojars." [_]
+  (println "Deploying to Clojars..")
+  (dd/deploy {:installer :remote
+              ;:sign-releases? true
+              :pom-file (b/pom-path (select-keys opts [:lib :class-dir]))
+              ;:artifact "target/tech.ml.dataset.jar"
+              :artifact (b/resolve-path jar-file)}))
 
 
