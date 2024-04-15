3ed4a225e5199090903971e684f461d62c9d9ea0
awb99 <awb99@gmail.com>
2024-04-14 22:17:21 -0500

    ci

--- build.clj
+++ build.clj
@@ -1,19 +1,14 @@
 (ns build
   (:require
-   [babashka.fs :as fs]
    [clojure.tools.build.api :as b]
    [org.corfield.build :as bb] ; https://github.com/seancorfield/build-clj
 ))
 
 (def lib 'org.pinkgorilla/goldly)
-(def version (format "0.5.%s" (b/git-count-revs nil)))
+(def version (format "0.7.%s" (b/git-count-revs nil)))
 
 (defn jar "build the JAR" [opts]
   (println "Building the JAR")
-  (spit (doto (fs/file "resources/META-INF/pink-gorilla/goldly/meta.edn")
-          (-> fs/parent fs/create-dirs)) {:module-name "goldly"
-                                          :version version
-                                          :generated (now-str)})
   (-> opts
       (assoc :lib lib
              :version version
