b57d43403340a993a669b2614c3b0b03caf364f7
awb99 <awb99@gmail.com>
2024-04-18 18:51:37 -0500

    cljs-service: add-cljs-kernel

---
+++ reval-sci/src/reval/init.cljs
@@ -0,0 +1,10 @@
+(ns reval.init
+   (:require
+    [taoensso.timbre :as timbre :refer [info warn]]
+    [webly.module.build :refer [load-namespace]]))
+
+(defn reval-cljs-kernel-init []
+  (info "goldly reval cljs-kernel-init ..")
+  (load-namespace 'reval.kernel.protocol)
+  (load-namespace 'reval.kernel.cljs-sci)
+  nil)
