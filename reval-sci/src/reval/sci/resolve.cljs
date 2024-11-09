b7987143a8875a17ae7b781367bd166830db31a4
awb99 <awb99@gmail.com>
2024-11-09 07:59:32 -0500

    reval kernel -> reval

--- reval-sci/src/reval/sci/resolve.cljs
+++
@@ -1,9 +0,0 @@
-(ns reval.goldly.viz.resolve
-  (:require
-   [goldly.sci :refer [require-async resolve-symbol-sci]]))
-
-(defn require-s-sci [s]
-  (let [libspec [(-> s namespace symbol)]
-        require-p (require-async libspec)]
-    (log "get-render-fn requiring libspec: " libspec)
-    (resolve s)))
